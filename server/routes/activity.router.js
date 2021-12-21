122a08b1905182af81b31b8d40e034d0cf8bc348
Paran Kashani <parankashani@gmail.com>
2021-12-20 18:46:38 -0600

    add new activity function works

--- server/routes/activity.router.js
+++ server/routes/activity.router.js
@@ -21,8 +21,42 @@ router.get('/', (req, res) => {
 /**
  * POST route template
  */
-router.post('/', (req, res) => {
-  // POST route code here
-});
+ router.post('/', (req, res) => {
+   //post route code goes here
+  console.log(req.body);
+  // RETURNING "id" will give us back the id of the created movie
+  const insertActivityQuery = `
+  INSERT INTO "activity" ("title", "description", "actors")
+  VALUES ($1, $2, $3)
+  RETURNING "id";`
+
+  // FIRST QUERY MAKES ACTIVITY
+  pool.query(insertActivityQuery, [req.body.title, req.body.description, req.body.actors])
+  .then(result => {
+    console.log('New activity Id:', result.rows[0].id); //ID IS HERE!
+    
+    const createdActivityId = result.rows[0].id
+
+    // Now handle the genre reference
+    const insertActivityTagQuery = `
+      INSERT INTO "activity_tag" ("activity_id", "tag_id")
+      VALUES  ($1, $2);
+      `
+      // SECOND QUERY ADDS TAG FOR THAT NEW ACTIVITY
+      pool.query(insertActivityTagQuery, [createdActivityId, req.body.tag_id]).then(result => {
+        //Now that both are done, send back success!
+        res.sendStatus(201);
+      }).catch(err => {
+        // catch for second query
+        console.log(err);
+        res.sendStatus(500)
+      })
+
+// Catch for first query
+  }).catch(err => {
+    console.log(err);
+    res.sendStatus(500)
+  })
+})
 
 module.exports = router;
